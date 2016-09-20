57c8fb21020f0ede19f3778cab2048985d5d2051
Şamil Altın <altinsamil@gmail.com>
2016-09-20 14:30:29 +0300

    Log4NetMVC - log4mongo-net
    log4mongo/log4mongo-net

---
+++ Log4NetMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using log4net.Config;
+
+namespace Log4NetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger logger;
+        public HomeController()
+        {
+            XmlConfigurator.Configure();
+            this.logger = new Logger(this.GetType().FullName);
+        }
+        public ActionResult Index()
+        {
+          
+            logger.Info("HOME");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            logger.Info("ABOUT");
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            logger.Info("CONTACT");
+
+            return View();
+        }
+    }
+}
